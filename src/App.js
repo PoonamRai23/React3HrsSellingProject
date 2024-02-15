5b224d3401ec8bc9de3f4797d2db9518376527d2
PoonamRai23 <128670417+PoonamRai23@users.noreply.github.com>
2024-02-15 15:45:23 +0530

    sellingproject

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Seller from "./Components/AdminPage";
+import DisplayProducts from "./Components/DisplayProducts";
 
 function App() {
+  const [totalExpense, setTotalExpense] = useState(0);
+  const [products, setProducts] = useState(
+    JSON.parse(localStorage.getItem("productData")) || []
+  );
+  const setDataInLocalStorage = (data) => {
+    const res = JSON.parse(localStorage.getItem("productData")) || [];
+    res.push(data);
+    localStorage.setItem("productData", JSON.stringify(res));
+    setProducts(res);
+    calculateTotalExpense();
+  };
+  const deleteProduct = (index) => {
+    // console.log("checking index", index);
+    const res = JSON.parse(localStorage.getItem("productData")) || [];
+    res.splice(index, 1);
+    // console.log("you have deleted", res);
+    setProducts(res);
+    localStorage.setItem("productData", JSON.stringify(res));
+    calculateTotalExpense();
+  };
+  const calculateTotalExpense = () => {
+    const res = JSON.parse(localStorage.getItem("productData")) || [];
+    let addition = 0;
+    for (let i = 0; i < res.length; i++) {
+      addition += parseInt(res[i].sellingPrice);
+    }
+    setTotalExpense(addition);
+  };
+  useEffect(() => {
+    calculateTotalExpense();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1>Seller's Admin Page</h1>
+        <Seller formSubmitted={setDataInLocalStorage} />
+        <h3>Total Expenses={totalExpense}</h3>
+        <DisplayProducts products={products} deleteItem={deleteProduct} />
+      </div>
+    </>
   );
 }
 
